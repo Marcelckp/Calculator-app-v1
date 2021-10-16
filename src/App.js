dcca8c8711ba829915241b040e2152e0f95362d3
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-10-16 17:00:17 +0200

    Added a calculations history toggle to view the calculations you made prior to clicking the toggle screen
    & also provided a catch all type conditional to make sure that you don't get an error when deleting and the result is displayed on the screen this catches that error

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import Buttons from './Components/Buttons/Buttons';
 
 function App() {
 
-  const [theme, setTheme] = useState(1)
+  const [theme, setTheme] = useState(1);
+  const [calSheetClicked, setSheetClicked] = useState(false)
 
   return (
     <div className={`App ${theme === '2' ? 'theme2' : theme === '3' ? 'theme3' : null}`}>
       <div className="calculatorBody">
         <div className={`calculatorHeader ${theme === '2' ? 'Theme-title-theme2' : theme === '3' ? 'Theme-title-theme3' : null}`}>
           <div className="title">
-            <h1>calc</h1>
+            <h1 onClick={() => setSheetClicked(false)}>calc</h1>
+          </div>
+          <div className='ThemeSlider'>
+            <p onClick={() => {
+              if (calSheetClicked) setSheetClicked(false);
+              else setSheetClicked(true);
+            }}>Calc History</p>
           </div>
           <div className="ThemeSlider">
             <p className={`Theme-Title`}>THEME</p>
@@ -23,8 +30,7 @@ function App() {
             </div>
           </div>
         </div>
-
-        <Buttons class={theme} />
+        <Buttons class={theme} calSheet={calSheetClicked} />
       </div>
     </div>
   );
--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -8,7 +8,11 @@ function Buttons(props) {
     const [calculation, setCalculation] = useState('');
     const [result, setResult] = useState(null);
 
+    const [completedCalculations, setCompletedCalculations] = useState([]);
+
     const operands = ['*', '+', '/', '-', '.'];
+
+    console.log(props.calSheet)
 
     const addToValStr = num => {
         if (num === '/' || num === '*' || num === '+' || num === '-' || num === '.'){
@@ -25,12 +29,13 @@ function Buttons(props) {
     }
 
     const deleteFromValStr = () => {
-        if (calculation.length === 0) {
+        if (calculation === '') {
             return;
         } else {
-            let val = calculation.split('');
+            let val = calculation.toString().split('')||0;
             val.pop();
             setCalculation(val.join(''));
+            setResult('');
         }
     }
 
@@ -43,69 +48,81 @@ function Buttons(props) {
         if (!result && !operands.includes(calculation.slice(-1))) {
             setResult(eval(calculation));
             setCalculation(eval(calculation));
+
+            setCompletedCalculations(prevVal => [...prevVal, calculation])
         } else if (!result && operands.includes(calculation.slice(-1))) {
             alert('Check Your calculation you evaluated a incomplete sum')
         }
-
-        // console.log(eval(calculation));
     }
 
-    // console.log(props.class)
+    console.log(completedCalculations)
 
     return (
-        <>
-        <div className={`Screen`}>
-            <div className={`Screen-container ${props.class === '2' ? 'Screen-container-theme2' : props.class === '3' ? 'Screen-container-theme3' : null}`}>
-                {/* <div>
-                    <h1 className="screen-previous"></h1>
-                </div> */}
-                {result || result === 0 ? 
-                    <div><h1 className="screenShape" style={{color: '#D03F30'}}>{result}</h1></div>
-                :
-                    <div><h1 className="screenShape">{calculation || 0}</h1></div>}
-                
-            </div>
+        <div>
+            {props.calSheet ? 
+            <div className={`calSheet-container ${props.class === '2' ? 'calSheet-container-th2' : props.class === '3' ? 'calSheet-container-th3' : null}`}>
+                <p>Calculation History</p>
+                <div className={`cal-showcase`}>
+                    {completedCalculations.map((arr, i) => {
+                        return <div key={i} className={`cal ${props.class === '2' ? 'cal-showcase-th2' : props.class === '3' ? 'cal-showcase-th3' : null}`}>
+                            <span className='indexNo'> ( {i} ) </span>
+                            <p>{arr} = <span>{ eval(arr) }</span></p>
+                        </div>
+                    })}
+                </div>
+            </div> :
+            <>
+                <div className={`Screen`}>
+                    <div className={`Screen-container ${props.class === '2' ? 'Screen-container-theme2' : props.class === '3' ? 'Screen-container-theme3' : null}`}>
+                        {result || result === 0 ? 
+                            <div><h1 className="screenShape" style={{color: '#D03F30'}}>{result}</h1></div>
+                        :
+                            <div><h1 className="screenShape">{calculation || 0}</h1></div>}
+                        
+                    </div>
+                </div>
+                <div className="Buttons">
+                    <div className={`Buttons-container ${props.class === '2' ? 'Buttons-container-theme2' : props.class === '3' ? 'Buttons-container-theme3' : null}`}>
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('7')}>7</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>8</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>9</button>
+
+                            <button className={`calc-Del ${props.class === '2' ? 'func-theme2' : props.class === '3' ? 'func-theme3' : null}`} onClick={(e) => deleteFromValStr()}>DEL</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>4</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>5</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>6</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>+</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>1</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>2</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>3</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`}onClick={() => addToValStr('-')}>-</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('.')}>.</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>0</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('/')}>/</button>
+
+                            <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('*')}>X</button>
+
+                            <button className={`ResetBtn ${props.class === '2' ? 'func-theme2' : props.class === '3' ? 'func-theme3' : null}`} onClick={() => resetCal()}>RESET</button>
+                            <button className={`EqualBtn ${props.class === '2' ? 'EqualBtn-theme2' : props.class === '3' ? 'EqualBtn-theme3' : null}`} onClick={() => equalsPressed()}>=</button>
+                        
+                    </div>
+                </div>
+            </>
+            }
         </div>
-        <div className="Buttons">
-            <div className={`Buttons-container ${props.class === '2' ? 'Buttons-container-theme2' : props.class === '3' ? 'Buttons-container-theme3' : null}`}>
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('7')}>7</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>8</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>9</button>
-
-                    <button className={`calc-Del ${props.class === '2' ? 'func-theme2' : props.class === '3' ? 'func-theme3' : null}`} onClick={(e) => deleteFromValStr()}>DEL</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>4</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>5</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>6</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>+</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>1</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>2</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>3</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`}onClick={() => addToValStr('-')}>-</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('.')}>.</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={(e) => addToValStr(e.target.innerText)}>0</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('/')}>/</button>
-
-                    <button className={`btn hov ${props.class === '2' ? 'btn-pad-theme2' : props.class === '3' ? 'btn-pad-theme3' : null}`} onClick={() => addToValStr('*')}>X</button>
-
-                    <button className={`ResetBtn ${props.class === '2' ? 'func-theme2' : props.class === '3' ? 'func-theme3' : null}`} onClick={() => resetCal()}>RESET</button>
-                    <button className={`EqualBtn ${props.class === '2' ? 'EqualBtn-theme2' : props.class === '3' ? 'EqualBtn-theme3' : null}`} onClick={() => equalsPressed()}>=</button>
-                
-            </div>
-        </div>
-        </>
     )
 }
 
